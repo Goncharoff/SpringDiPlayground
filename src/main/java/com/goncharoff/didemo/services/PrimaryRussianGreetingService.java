0c89a839983acdb0c3cae1fb73c87d62332d4aac
Nik <nik.ignition.work@gmail.com>
2020-01-26 00:30:14 +0200

    lessonb 42, added jp greeting and english now default profile

--- src/main/java/com/goncharoff/didemo/services/PrimaryRussianGreetingService.java
+++ src/main/java/com/goncharoff/didemo/services/PrimaryRussianGreetingService.java
@@ -1,4 +1,16 @@
 package com.goncharoff.didemo.services;
 
-public class PrimaryRussianGreetingService {
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("ru")
+@Primary
+public class PrimaryRussianGreetingService implements GreetingService{
+
+    @Override
+    public String sayHello() {
+        return "Приветик с ру праймал профайла";
+    }
 }
