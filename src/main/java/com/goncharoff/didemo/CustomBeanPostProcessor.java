7c1810139696c29205688408fc1692423b7b789a
Nik <nik.ignition.work@gmail.com>
2020-01-26 01:08:36 +0200

    added classes for viewing spring beans lifecycle

---
+++ src/main/java/com/goncharoff/didemo/CustomBeanPostProcessor.java
@@ -0,0 +1,4 @@
+package com.goncharoff.didemo;
+
+public class CustomBeanPostProcessor {
+}
--- src/main/java/com/goncharoff/didemo/services/PrimaryJapanGreetingService.java
+++ src/main/java/com/goncharoff/didemo/services/PrimaryJapanGreetingService.java
@@ -1,4 +1,16 @@
 package com.goncharoff.didemo.services;
 
-public class PrimaryJapanGreetingService {
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile("jp")
+public class PrimaryJapanGreetingService implements GreetingService {
+
+    @Override
+    public String sayHello() {
+        return "おはよお！";
+    }
 }
